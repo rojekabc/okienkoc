ef61b542de60b8c97e1e7ef0569ec3dbf6ca8cf5
rojekabc <rojek_abc@o2.pl>
2014-01-11 11:11:43 +0100

    Clean warnings and errors. Remove fobobject project files - deprecated

--- src/err.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GOC_ERR_H_
-#define _GOC_ERR_H_
-
-// Zly argument + numer argumentu liczac od zera
-#define GOC_ERR_WRONGARGUMENT 10
-#define GOC_ERR_WRONGFILEDATA 20
-#define GOC_ERR_FILEEXIST 30
-
-#endif // _GOC_ERR_H_
